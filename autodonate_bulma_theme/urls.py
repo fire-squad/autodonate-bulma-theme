803f905a30a22e74761e48377b6faf015a8274d9
cofob <cofob@riseup.net>
2022-02-23 20:02:12 +0700

    Update

--- autodonate_bulma_theme/urls.py
+++ autodonate_bulma_theme/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from autodonate_bulma_theme import views
 
-
 urlpatterns = [
     path('', views.index, name='index')
 ]
--- autodonate_bulma_theme/views.py
+++ autodonate_bulma_theme/views.py
@@ -1,5 +1,5 @@
-from django.http.response import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse(b"ok")
+    return render(request, "bulma/pages/index.html")
